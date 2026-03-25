65964472ee4e04bd7530cf215d22384be2592eed
Eromab12 <98437813+Eromab12@users.noreply.github.com>
2026-03-25 19:22:56 -0400

    change

--- apiConfig.js
+++ apiConfig.js
@@ -1,8 +1,3 @@
-// Asegúrate de que esta dirección IP sea la correcta para tu computadora
-// donde se está ejecutando el backend.
-// Puedes https://gymapp-backend.onrender.com encontrarla ejecutando 'ipconfig' (en Windows) o 'ifconfig' (en macOS/Linux) en tu terminal.
-// --- URL DE PRODUCCIÓN ---
-// Reemplaza esta URL con la que te proporcionó Render para tu Web Service.
-const API_BASE_URL = 'https://gymapp-backend-brfz.onrender.com'; // <-- URL de la APi RENDER
+const API_BASE_URL = 'example.com'; // <-- URL de la APi RENDER
 
 export const API_URL = `${API_BASE_URL}/api/auth`;
